02f98ecfda572acce2b233c846aadcae5f55dfcf
Ha Le <nhiha001@gmail.com>
2022-03-09 14:31:35 +0700

    set map rvm

--- Capfile
+++ Capfile
@@ -34,6 +34,7 @@ require "capistrano/rails/assets"
 require "capistrano/rails/migrations"
 # require "capistrano/passenger"
 require 'capistrano/puma'
+
 install_plugin Capistrano::Puma  # Default puma tasks
 install_plugin Capistrano::Puma::Workers  # if you want to control the workers (in cluster mode)
 install_plugin Capistrano::Puma::Jungle # if you need the jungle tasks
--- config/deploy.rb
+++ config/deploy.rb
@@ -9,8 +9,8 @@ set :use_sudo, true
 set :branch, 'master'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :linked_files, %w{config/master.key}
+set :rvm_map_bins, %w{gem rake ruby rails bundle}
 require "rvm/capistrano"
-
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
