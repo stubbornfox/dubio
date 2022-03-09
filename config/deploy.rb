2b572c72eb01173af2d734857328cfe2d1451ef3
Ha Le <nhiha001@gmail.com>
2022-03-09 14:28:01 +0700

    [f] Add config deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :use_sudo, true
 set :branch, 'master'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :linked_files, %w{config/master.key}
+require "rvm/capistrano"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
