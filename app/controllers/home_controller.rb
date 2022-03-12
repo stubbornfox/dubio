e669c7258f52321072566cb456105c254b2752c2
Ha Le <nhiha001@gmail.com>
2022-03-12 17:16:29 +0700

    [f] test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @dict = Dict.my_dict
-    @cat_breeds = CatBreed.all
+    # @dict = Dict.my_dict
+    # @cat_breeds = CatBreed.all
   end
 end
