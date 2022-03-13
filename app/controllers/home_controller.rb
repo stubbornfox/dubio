84bd03b399f363eb46350a0b7d2b2cf3f1409966
Ha Le <nhiha001@gmail.com>
2022-03-13 18:23:26 +0700

    [f] puts ENV

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    puts "---------------"
+    puts ENV['RAILS_ENV']
+    puts "---------------"
     @dict = Dict.my_dict
     @cat_breeds = CatBreed.all
   end
