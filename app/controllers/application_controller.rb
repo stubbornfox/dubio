2eb731f8b99ddee6b019059a4df20a5cefd20dfc
Ha Le <nhiha001@gmail.com>
2022-03-13 16:58:51 +0700

    http authentication for production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: Rails.application.credentials.HTTP_BASIC_AUTH_USER, password: Rails.application.credentials.HTTP_BASIC_AUTH_PASSWORD, if: -> { ENV['RAILS_ENV'] == 'production' }
 end
